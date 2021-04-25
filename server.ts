c4757822034cc892410a43c0073017a5e1868847
luis-dot-lopes <luisantonio20054@gmail.com>
2021-04-25 15:28:29 -0300

    Added typescript and routes.ts. Planning on adding login page

---
+++ server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import path from 'path';
+import { router } from "./routes";
+const app = express();
+
+const port = 3000;
+
+app.use(router);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set("views", path.join(__dirname, 'public'));
+
+app.engine("html", require('ejs').renderFile);
+
+app.set("view engine", "html");
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
