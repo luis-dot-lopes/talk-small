e3139e79e7c33f7facb032768968855e5fd3a252
Jairo Silva <70125199+jairosilva2005@users.noreply.github.com>
2021-06-27 21:49:39 -0300

    Restore repository

--- api.js
+++
@@ -1,8 +0,0 @@
-import openSocket from 'socket.io-client';
-const socket = openSocket('https://localhost:3001');
-
-function communicateWithApi(cb) {
-	socket.on();
-}
-
-export { communicateWithApi };
