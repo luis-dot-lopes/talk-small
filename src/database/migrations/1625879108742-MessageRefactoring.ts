a25d789e8aeb1a4727d2f5e3e087eb2264170d57
luis-dot-lopes <luisantonio20054@gmail.com>
2021-07-11 16:02:06 -0300

    Added script to automatically create talks

--- src/database/migrations/1625879108742-MessageRefactoring.ts
+++ src/database/migrations/1625879108742-MessageRefactoring.ts
@@ -1,4 +1,7 @@
-import {Column, MigrationInterface, QueryRunner, Table, TableColumn, TableForeignKey} from "typeorm";
+import {Column, getCustomRepository, MigrationInterface, QueryRunner, Repository, TableColumn, TableForeignKey} from "typeorm";
+import { Message } from "../../entities/Message";
+import { MessagesRepository } from "../../repositories/MessagesRepository";
+import { TalksService } from "../../services/TalksService";
 
 export class MessageRefactoring1625879108742 implements MigrationInterface {
 
@@ -19,11 +22,28 @@ export class MessageRefactoring1625879108742 implements MigrationInterface {
                 onUpdate: "SET NULL",
             })
         );
+
+        const messagesRepository: Repository<Message> = getCustomRepository(MessagesRepository);
+        const talksService = new TalksService();
+        const allMessages = await messagesRepository.find({
+            select: ["sender_id", "receiver_id", "id"]
+        });
+
+        for(let { sender_id, receiver_id, id } of allMessages) {
+            const talk = await talksService.create({
+                user_id1: sender_id, 
+                user_id2: receiver_id,
+            });
+
+            await messagesRepository.update({ id }, {talk_id: talk.id });
+        }
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.dropForeignKey("messages", "FKTalk");
-        queryRunner.dropColumn("messages", "talk_id");
+        await queryRunner.dropForeignKey("messages", "FKTalk");
+        await queryRunner.dropColumn("messages", "talk_id");
+        await queryRunner.clearTable("talks");
     }
 
 }
