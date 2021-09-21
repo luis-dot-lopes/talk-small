cbf1971f1ebcdb5704b267e397fabc724e359d8b
luis-dot-lopes <luisantonio20054@gmail.com>
2021-09-21 11:21:08 -0300

    Added routes for the talk controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
 import { MessagesController } from "./controller/MessagesController";
+import { TalksController } from "./controller/TalksController";
 import { UsersController } from "./controller/UsersController";
 
 const router = Router();
 
 const usersController = new UsersController();
 const messagesController = new MessagesController();
-
-router.get("/", (req, res) => {
-    res.render("login.html");
-});
+const talksController = new TalksController();
 
 router.get("/talks", (req, res) => {
     res.render("index.html");
@@ -21,5 +19,8 @@ router.post("/signup", usersController.create);
 router.post("/login", usersController.loginIn);
 router.post("/send", messagesController.createFromReq);
 router.post("/listMessages", messagesController.listByUser);
+
+router.post("/createTalk", talksController.createFromReq);
+router.post("/listTalks", talksController.listByUser);
 
 export { router };
